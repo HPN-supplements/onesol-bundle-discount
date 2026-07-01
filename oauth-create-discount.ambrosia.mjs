18283854fe9cca9c478dc2374bc3d1eafe6d61b4
pabloamicodev <Pablo.Amico.CFT@jcrew.com>
2026-06-30 21:59:05 -0300

    fix

--- oauth-create-discount.ambrosia.mjs
+++ oauth-create-discount.ambrosia.mjs
@@ -102,7 +102,7 @@ async function createDiscountWithGid(token, functionGid) {
         discountClasses: [PRODUCT]
         startsAt: "2026-01-01T00:00:00Z"
         combinesWith: {
-          productDiscounts: false
+          productDiscounts: true
           orderDiscounts: true
           shippingDiscounts: true
         }
